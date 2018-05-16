032bba418425ba2513241d2f9e4970ca85b71391
Bert Kleewein <bertk@microsoft.com>
2018-05-16 13:16:42 -0700

    checkpoint

--- testapp/build.sh
+++ testapp/build.sh
@@ -1,8 +1,11 @@
 
-clang++ \
+g++ \
     --debug \
+    -g \
     -I ../deps/restbed/distribution/include \
     -L ../deps/restbed/distribution/library \
     -std=gnu++11 \
     -o example iotapp.cpp DefaultApi.cpp \
     -l restbed -l crypto -l pthread 
+
+    # -D_GLIBCXX_DEBUG \
